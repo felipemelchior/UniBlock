99dee7b7317463c44b4e24c2e576ab1eee2fe44e
Felipe Homrich Melchior <fehmel@gmail.com>
2019-05-07 19:39:05 -0300

    feat: clean useless code

--- comunnication.py
+++ comunnication.py
@@ -1,19 +1,18 @@
-import os, sys, getopt
 import socket
 import threading
-import logging
-
-logging.basicConfig(level=logging.INFO, format='%(levelname)s:%(threadName)s:%(message)s')
 
 class Connection:
     def __init__(self, myIp):
         self.myIp=myIp
+
     @property
     def myIp(self):
         return self._myIp
+    
     @myIp.setter
     def myIp(self, value):
         self._myIp=value
+
     def listenConnection(self, port=5055):
         '''
         Coloca o servidor para rodar de fato
@@ -28,28 +27,28 @@ class Connection:
                 server.bind((self._myIp, port))
                 server.listen(10)
             except:
-                logging.info(" Error on start server")
+                print("Error on start server")
     
-            logging.info(" WebServer running on port {0}".format(port))
+            print("WebServer running on port {0}".format(port))
 
             threads = []
 
             try:
                 while True:
                     conn, addr = server.accept()
-                    logging.info(" New Connection from " + str(addr[0]) + " with port " + str(addr[1]))
+                    print(" New Connection from " + str(addr[0]) + " with port " + str(addr[1]))
                     
                     aux = threading.Thread(target=connected, args=(conn,addr))
                     aux.setDaemon(True)				
                     aux.start()
                     threads.append(aux)
             except:
-                logging.info(" Ending the server execution")
+                print("Ending the server execution")
 
             server.close()
 
         except (KeyboardInterrupt, SystemExit):
-            logging.info(" Finishing execution of WebServer...")
+            print("Finishing execution of WebServer...")
             exit()
 
 class Minner(Connection):
